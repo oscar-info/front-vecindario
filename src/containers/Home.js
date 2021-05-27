9f08e6668f95bc31bd8cf79eebb73227065cf552
oscar-info <oscar.info@vecindario.com>
2021-05-27 14:35:07 -0500

    adding the user creation and login methods

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -15,9 +15,10 @@ function Home() {
   const [setModalType] = useState(" ");
   const [setModalIsOpen] = useState(false);
   const [leads] = useState([]);
+  const [currentUser, setCurrentUser] = useState([]);
+  const [users] = useState([]);
   const { addToast } = useToasts()
 
-  console.log(projectId)
 
   function openModal(type) {
     setModalType(type);
@@ -38,11 +39,43 @@ function Home() {
       addToast("Su informacion fue enviada exitosamente", {
         appearance: 'success',
         autoDismiss: true,
-    })
-    history.push('/')
+      })
+      history.push('/')
     })
     .catch(() => {
       addToast("Su informacion no pudo ser enviada", {
+        appearance: 'error',
+        autoDismiss: true,
+    })})
+  };
+
+  const login = (data) => {
+    console.log(data)
+    axios.post("http://localhost:3000/auth/login", data)
+    .then((response) => {
+      setCurrentUser(response.data)
+      history.push('/cms')
+    })
+    .catch(() => {
+      addToast("El email o contraseña es invalido", {
+        appearance: 'error',
+        autoDismiss: true,
+    })})
+    console.log("current user: " + currentUser)
+  };
+
+  const createUser = (data) => {
+    axios.post("http://localhost:3000/users", data)
+    .then((response) => {
+      users.push(response.data);
+      addToast("El usuario fue creado exitosamente", {
+        appearance: 'success',
+        autoDismiss: true,
+      })
+      history.push('/cms')
+    })
+    .catch(() => {
+      addToast("Su cuenta no pudo ser creada, verifique todos los campos", {
         appearance: 'error',
         autoDismiss: true,
     })})
@@ -63,11 +96,14 @@ function Home() {
       <Footer />
       <Route
         exact
-        path="/ingresar"
+        path="/auth/login"
         component={() => (
           <FormModal
             type={'login'}
             isOpen={true}
+            onSubmit={(data) => {
+              login(data);
+            }}
             onRequestClose={()=> history.push('/')}
           />
         )}
@@ -79,6 +115,9 @@ function Home() {
           <FormModal
             type={'register'}
             isOpen={true}
+            onSubmit={(data) => {
+              createUser(data);
+            }}
             onRequestClose={()=> history.push('/')}
           />
         )}
